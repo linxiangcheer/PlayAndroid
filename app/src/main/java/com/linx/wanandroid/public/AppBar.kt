1650f231cf3772551b234d735388e7d3aca795dc
linxiang <1073456077@qq.com>
2021-08-26 22:31:05 +0800

    TopBar布局问题修复

--- app/src/main/java/com/linx/wanandroid/public/AppBar.kt
+++ app/src/main/java/com/linx/wanandroid/public/AppBar.kt
@@ -1,13 +1,13 @@
 package com.linx.wanandroid.public
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextAlign
@@ -33,31 +33,41 @@ fun AppBar(
     TopAppBar(
         backgroundColor = MaterialTheme.colors.primary
     ) {
-        Row {
-            //左边图标
-            leftIcon?.let {
-                Icon(leftIcon, contentDescription = null, modifier = Modifier.clickable {
-                    onLeftClick()
-                }.padding(start = 10.dp), tint = MaterialTheme.colors.background)
+
+        Box(
+            contentAlignment = Alignment.Center
+        ) {
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                //左边图标
+                if (leftIcon == null)
+                    Spacer(modifier = Modifier.size(10.dp))
+                else
+                    Icon(leftIcon, contentDescription = null, modifier = Modifier.clickable {
+                        onLeftClick()
+                    }.padding(start = 10.dp), tint = MaterialTheme.colors.background)
+
+                //右边图标
+                rightIcon?.let {
+                    Icon(rightIcon, contentDescription = null, Modifier.clickable {
+                        onRightClick()
+                    }.padding(end = 10.dp), tint = MaterialTheme.colors.background)
+                }
             }
 
             //标题文字
             Text(
                 text = title,
-                modifier = Modifier.weight(1f),
                 textAlign = TextAlign.Center,
                 fontSize = 18.sp,
                 color = MaterialTheme.colors.background
             )
-
-            //右边图标
-            rightIcon?.let {
-                Icon(rightIcon, contentDescription = null, Modifier.clickable {
-                    onRightClick()
-                }.padding(end = 10.dp), tint = MaterialTheme.colors.background)
-            }
-
         }
+
+
     }
 
 }

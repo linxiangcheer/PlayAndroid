d58862e0939b78e085c1e16c2bb45894efaf2c28
linxiang <1073456077@qq.com>
2021-08-29 22:18:25 +0800

    首页数据优化

--- app/src/main/java/com/linx/playAndroid/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/linx/playAndroid/viewModel/HomeViewModel.kt
@@ -3,10 +3,13 @@ package com.linx.playAndroid.viewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.linx.common.base.BaseViewModel
 import com.linx.common.paging.CommonPagingSource
+import com.linx.playAndroid.model.ArticleListData
 import com.linx.playAndroid.repo.HomeRepo
+import kotlinx.coroutines.flow.Flow
 
 /**
  * 首页
@@ -14,7 +17,10 @@ import com.linx.playAndroid.repo.HomeRepo
 class HomeViewModel: BaseViewModel() {
 
     //首页列表
-    val homeListData = Pager(PagingConfig(pageSize = 20)) {
+    val homeListData: Flow<PagingData<ArticleListData>>
+        get() = _homeListData
+
+    private val _homeListData = Pager(PagingConfig(pageSize = 20)) {
         CommonPagingSource { nextPage: Int ->
             HomeRepo.getHomeList(nextPage)
         }

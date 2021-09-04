c866069256a17c33401faae66b2666e15360c628
linxiang <1073456077@qq.com>
2021-09-04 11:05:19 +0800

    新增DataStore来达到Cookie持久化

--- app/src/main/java/com/linx/playAndroid/initializer/WidgetInitializer.kt
+++ app/src/main/java/com/linx/playAndroid/initializer/WidgetInitializer.kt
@@ -3,6 +3,7 @@ package com.linx.playAndroid.initializer
 import android.content.Context
 import android.util.Log
 import androidx.startup.Initializer
+import com.linx.net.widget.DataStoreUtils
 
 /**
  * 初始化工具类
@@ -10,6 +11,8 @@ import androidx.startup.Initializer
 class WidgetInitializer: Initializer<Boolean> {
 
     override fun create(context: Context): Boolean {
+        Log.i("初始化", "初始化DataStore")
+        DataStoreUtils.init(context)
         return true
     }
 
--- net/src/main/java/com/linx/net/base/ServiceCreator.kt
+++ net/src/main/java/com/linx/net/base/ServiceCreator.kt
@@ -1,6 +1,6 @@
 package com.linx.net.base
 
-import com.linx.net.cookie.LocalCookieJar
+import com.linx.net.widget.DataStoreUtils
 import com.linx.net.widget.KtHttpLogInterceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -11,6 +11,11 @@ import java.util.concurrent.TimeUnit
  * retrofit2请求
  */
 object ServiceCreator {
+
+    private const val SAVE_USER_LOGIN_KEY = "user/login"
+    private const val SAVE_USER_REGISTER_KEY = "user/register"
+    private const val SET_COOKIE_KEY = "set-cookie"
+    private const val COOKIE_NAME = "Cookie"
 
     private val mOkClient: OkHttpClient = OkHttpClient.Builder()
         //完整请求超时时长，从发起到接受返回数据，默认0s
@@ -25,13 +30,54 @@ object ServiceCreator {
         .retryOnConnectionFailure(true)
         //是否重定向
         .followRedirects(false)
-        //cookie持久化处理
-        .cookieJar(LocalCookieJar())
+        //保存cookie
+        .addInterceptor {
+            val request = it.request()
+            val response = it.proceed(request)
+            val requestUrl = request.url.toString()
+            val domain = request.url.host
+            //cookie可能有多个，都保存下来
+            if ((requestUrl.contains(SAVE_USER_LOGIN_KEY) || requestUrl.contains(
+                    SAVE_USER_REGISTER_KEY
+                )) && response.headers(SET_COOKIE_KEY).isNotEmpty()
+            ) {
+                val cookies = response.headers(SET_COOKIE_KEY)
+                val cookie = encodeCookie(cookies)
+                saveCookie(requestUrl, domain, cookie)
+            }
+            response
+        }
+        //请求时设置cookie
+        .addInterceptor {
+            val request = it.request()
+            val builder = request.newBuilder()
+            val domain = request.url.host
+            //获取domain内的cookie
+            if (domain.isNotEmpty()) {
+                val sqDomain: String = DataStoreUtils.readStringData(domain, "")
+                val cookie: String = if (sqDomain.isNotEmpty()) sqDomain else ""
+                if (cookie.isNotEmpty()) {
+                    builder.addHeader(COOKIE_NAME, cookie)
+                }
+            }
+            it.proceed(builder.build())
+        }
         //添加网络拦截器，打印日志
         .addNetworkInterceptor(KtHttpLogInterceptor {
             logLevel(KtHttpLogInterceptor.LogLevel.BODY)
         })
         .build()
+
+    /**
+     * 保存cookie
+     */
+    @Suppress("ASSIGNED_BUT_NEVER_ACCESSED_VARIABLE")
+    private fun saveCookie(url: String?, domain: String?, cookies: String) {
+        url ?: return
+        DataStoreUtils.putSyncData(url, cookies)
+        domain ?: return
+        DataStoreUtils.putSyncData(domain, cookies)
+    }
 
     private var retrofit: Retrofit? = null
 
@@ -61,4 +107,32 @@ object ServiceCreator {
         }
     }
 
+}
+
+/**
+ * 整理cookie
+ */
+fun encodeCookie(cookies: List<String>): String {
+    val sb = StringBuilder()
+    val set = HashSet<String>()
+    cookies
+        .map { cookie ->
+            cookie.split(";".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        }
+        .forEach { it ->
+            it.filterNot { set.contains(it) }.forEach { set.add(it) }
+        }
+
+    val ite = set.iterator()
+    while (ite.hasNext()) {
+        val cookie = ite.next()
+        sb.append(cookie).append(";")
+    }
+
+    val last = sb.lastIndexOf(";")
+    if (sb.length - 1 == last) {
+        sb.deleteCharAt(last)
+    }
+
+    return sb.toString()
 }
